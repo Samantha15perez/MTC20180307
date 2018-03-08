0458e8232f915cb53506fa93f537ba5b9bb15dbc
Samantha15perez <Samantha15perez@gmail.com>
2018-03-07 21:09:19 -0500

    03072018

---
+++ ClassworkSamanthaPerez03072018/ClassworkSamanthaPerez03072018/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ClassworkSamanthaPerez03072018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.theBigBadDataGrid = new System.Windows.Forms.DataGridView();
+            this.dataLoadButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.theBigBadDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // theBigBadDataGrid
+            // 
+            this.theBigBadDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.theBigBadDataGrid.Location = new System.Drawing.Point(7, 10);
+            this.theBigBadDataGrid.Name = "theBigBadDataGrid";
+            this.theBigBadDataGrid.Size = new System.Drawing.Size(761, 205);
+            this.theBigBadDataGrid.TabIndex = 0;
+            this.theBigBadDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataLoadButton
+            // 
+            this.dataLoadButton.Location = new System.Drawing.Point(289, 238);
+            this.dataLoadButton.Name = "dataLoadButton";
+            this.dataLoadButton.Size = new System.Drawing.Size(75, 23);
+            this.dataLoadButton.TabIndex = 1;
+            this.dataLoadButton.Text = "Load Data";
+            this.dataLoadButton.UseVisualStyleBackColor = true;
+            this.dataLoadButton.Click += new System.EventHandler(this.dataLoadButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(408, 238);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 273);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.dataLoadButton);
+            this.Controls.Add(this.theBigBadDataGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.theBigBadDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView theBigBadDataGrid;
+        private System.Windows.Forms.Button dataLoadButton;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ ClassworkSamanthaPerez03072018/ClassworkSamanthaPerez03072018/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+//note: remember to slap that last one in
+
+namespace ClassworkSamanthaPerez03072018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataLoadButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+           
+            //Define connection string, see connectionstrings.com for reference
+            string connString = @"Server=PL8\MTCDB; Database=Adventureworks2012; Trusted_Connection=True;";
+
+            // Define the connection using the connection string.
+            SqlConnection sqlConn = new SqlConnection(connString);
+
+            // Define a data adapter to pull the data from the server using the connection and a stored procedure.
+            SqlDataAdapter sqlDA = new SqlDataAdapter("NameRetrieval", sqlConn);
+
+            // Declare an empty data table to hold the data.
+            DataTable dtVendors = new DataTable();
+
+            // Fill the data table using the data adapter.
+            sqlDA.Fill(dtVendors);
+
+            // Use the data table as the data source for a data grid control.
+            theBigBadDataGrid.DataSource = dtVendors;
+            }
+
+            catch
+            {
+                MessageBox.Show("Oops, something went wrong");
+            }
+        }
+    }
+}
